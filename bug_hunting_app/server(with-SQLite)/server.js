5bf8141f5b3e61106de8db29fc95630945356e84
VisanLarisa <82821372+VisanLarisa@users.noreply.github.com>
2022-02-22 16:10:52 +0200

    clean

--- bug_hunting_app/server(with-SQLite)/server.js
+++ bug_hunting_app/server(with-SQLite)/server.js
@@ -1,35 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const crypto = require('crypto')        // 4 tokens
-// const secret = "A bear walks into a bar and says, 'Give me a whiskey and … cola.''Why the big pause?'asks the bartender. " + 
-// "The bear shrugged. 'I m not sure; I was born with them.'"
-//const sha256Hasher = crypto.createHmac("sha256", secret)
+const crypto = require('crypto')      
 
-// const bcrypt = require("bcrypt")
-// const saltRounds = 10;
-
-const moment = require('moment')        // for token expiration
+const moment = require('moment')    
 var Sequelize = require('sequelize');
-//const { body, validationResult } = require('express-validator');  //modificare - comentat
 
-// ONE WAY TO VALIDATE EMAIL - REGEX
+
 function validateEmail(email) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
 
-//misplaced for SQLite
-// const DB_NAME = 'bugtracking'
-// const DB_USER = 'webtechMaster'
-// const DB_PASSWORD = 'webtechMaster'
-// const TOKEN_TIMEOUT = 600
-// const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-//     host: '127.0.0.1',
-//     port: 3306,
-//     dialect: 'mysql'
-// })
-
-//missplaced with
 const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: 'sample.db',
@@ -38,13 +19,10 @@ const sequelize = new Sequelize({
     }
   })
 
-//and added
+
 const Op=Sequelize.Op
 const cors = require('cors')
-//end of modiffications
 
-
-// tables
 const User = sequelize.define('users', {
     email: Sequelize.STRING,
     password: Sequelize.STRING,
@@ -57,7 +35,7 @@ const Project = sequelize.define('projects', {
     description: Sequelize.TEXT
 })
 
-// Bug-ul conține o severitate, o prioritate de rezolvare, o descriere și un link la commit-ul
+
 const Bug = sequelize.define('bugs', {
     status: Sequelize.STRING,
     severity: Sequelize.STRING,
@@ -71,39 +49,30 @@ const Permission = sequelize.define('permission', {
     permType: Sequelize.ENUM('read')
 })
 
-// this are for permission stuff
+
 User.hasMany(Permission)
 Project.hasMany(Permission)
-// Resource.hasMany(Permission)
 
-// this is for joins
 Project.hasMany(User)
 Project.hasMany(Bug)
 
 
 const app = express();
-// this is the middleware for content
-app.use(express.json())
-//app.use(bodyParser.json())
 
-//adaugari -modificari
+app.use(express.json())
+
 app.use(cors())
 app.use(bodyParser.json())
-//sf modif
 
-
-// THESE SHOULD BE CHANGED
-const authRouter = express.Router() // login
-const adminRouter = express.Router()    // ignore 4 now
-const apiRouter = express.Router()  // protected part of the app
+const authRouter = express.Router()
+const adminRouter = express.Router()  
+const apiRouter = express.Router()  
 
 app.use('/auth', authRouter)
 app.use('/admin', adminRouter)
 app.use('/api', apiRouter)
 
 
-
-// TODO - this should be changed for one of our resources
 const permMiddleWare = async (req, res, next) => {
     const user = res.locals.user
     if (user.id != req.params.uid) {
@@ -131,7 +100,6 @@ const permMiddleWare = async (req, res, next) => {
 }
 
 
-// in order to check tokens api router
 apiRouter.use(async (req, res, next) => {
     const token = req.headers.auth
     if (token) {
@@ -161,7 +129,7 @@ apiRouter.use(async (req, res, next) => {
     }
 })
 
-// No duplicate entried - DONE
+
 adminRouter.post('/users', async (req, res) => {
     try {
         if (validateEmail(req.body.email) && req.body.password.length > 6) {
@@ -188,9 +156,9 @@ adminRouter.post('/users', async (req, res) => {
 
 })
 
-// ---------------------------------------------LOGIN and token retrieval------------------------------------------
 
-// TODO email function node.js
+
+
 authRouter.post('/login', async (req, res) => {
     try {
         const credentials = req.body
@@ -222,13 +190,11 @@ authRouter.get('/logout', function(req, res) {
     res.status(200).json({ message: 'logged out' })
 })
 
-// a fost nevoie sa definesc asta aici, pentru a crea baza de date in cazul in care nu o am deja (la prima rulare)
-// sequelize.sync({force: true})   //forteaza 
+// if you don't have the database already created, it could be needed for you to run this piece of code at first run, instead of the "app.get('/sync', async (req, res)" that follows 
+// sequelize.sync({force: true})  
 // .then(()=>console.log('created'))
 // .catch((error)=>console.log(error));
 
-
-// --------------------------------------------RESET DATABASE-------------------------------------------------
 app.get('/sync', async (req, res) => {
     try {
         await sequelize.sync({ force: true })
@@ -240,7 +206,6 @@ app.get('/sync', async (req, res) => {
 })
 
 
-// -------------------------------------------PROJECT REQUESTS------------------------------------------------
 app.get('/projects', async (req, res) => {
     try {
         const projects = await Project.findAll({ include: [Bug, User] })
@@ -350,9 +315,7 @@ apiRouter.post('/projects/:pid/users', async (req, res) => {
     }
 })
 
-//------------------------------------------------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx----------------------------------------------------
 
-// here - from the moment one adds a project, that project is his      -- I  think this should be changed - no duplicates check by repo name
 apiRouter.post('/users/:uid/projects', async (req, res, next) => {
     try {
         const user = await User.findByPk(req.params.uid)
@@ -385,7 +348,6 @@ apiRouter.post('/users/:uid/projects', async (req, res, next) => {
     }
 })
 
-// TEST TEST TEST
 apiRouter.get('/users/:uid/projects/:pid', permMiddleWare, async (req, res, next) => {
     try {
         const project = await Project.findByPk(req.params.pid)
@@ -401,8 +363,6 @@ apiRouter.get('/users/:uid/projects/:pid', permMiddleWare, async (req, res, next
 })
 
 
-
-// show all projects related to connected 
 apiRouter.get('/users/:uid/projects', async (req, res, next) => {
     try {
         const user = await User.findByPk(req.params.uid)
@@ -423,7 +383,6 @@ apiRouter.get('/users/:uid/projects', async (req, res, next) => {
     }
 })
 
-// if tester show all projects
 app.get('/projects', async (req, res) => {
     try {
         const projects = await Project.findAll({ include: [Bug, User] })
@@ -435,7 +394,6 @@ app.get('/projects', async (req, res) => {
 })
 
 
-// if not connected to project in db - PUT /projects/:pid/ - verify if tester or dev
 
 apiRouter.put('/users/:uid/projects/:pid', async (req, res, next) => {
     try {
@@ -444,8 +402,8 @@ apiRouter.put('/users/:uid/projects/:pid', async (req, res, next) => {
         if (user !== null && project != null) {
             const token = req.headers.auth
             if (user.token === token) {
-                if (user.projectId === project.id) {            // TODO - this should be changed => let user change repo as well
-                    //await project.update(req.body, { fields: ['repository', 'description']})    // if let change both fields
+                if (user.projectId === project.id) {            
+                    //await project.update(req.body, { fields: ['repository', 'description']})  
                     project.description = req.body.description
                     project.save()
                     res.status(202).json({ message: 'accepted' })
@@ -471,7 +429,7 @@ apiRouter.delete('/users/:uid/projects/:pid', async (req, res, next) => {
         if (user !== null && project != null) {
             const token = req.headers.auth
             if (user.token === token) {
-                if (user.projectId === project.id) {            // TODO - this should be changed => let user change repo as well
+                if (user.projectId === project.id) {    
                     await project.destroy()    // if let change both fields
                     res.status(202).json({ message: 'accepted' })
                 } else {
@@ -490,8 +448,6 @@ apiRouter.delete('/users/:uid/projects/:pid', async (req, res, next) => {
 
 
 
-// if not connected to project in db - POST /projects/:pid/ - req body cu bug status description - if bug
-//          auto add as pending
 // apiRouter.post('/users/:uid/projects/:pid/bugs', async (req, res, next) => {
 //     try {
 //         const user = await User.findByPk(req.params.uid)
@@ -524,7 +480,7 @@ apiRouter.delete('/users/:uid/projects/:pid', async (req, res, next) => {
 //     }
 // })
 
-//added - modification
+//temporarly misplaced by
 app.post('/projects/pid/bugs', async (req, res) => {
     try {
       const project = await Article.findByPk(req.params.pid)
@@ -545,7 +501,7 @@ app.post('/projects/pid/bugs', async (req, res) => {
   })
 
 
-// let members edit bugs - "SOLVE them" -> Testers can't change their status, they can only add them
+
 apiRouter.put('/users/:uid/projects/:pid/bugs/:bid', async (req, res, next) => {
     try {
         const user = await User.findByPk(req.params.uid)
@@ -554,17 +510,13 @@ apiRouter.put('/users/:uid/projects/:pid/bugs/:bid', async (req, res, next) => {
         if (user != null && project != null) {
             const token = req.headers.auth
             if (user.token === token) {
-                // if the user is a member of the project
                 if (user.projectId === project.id) {
-                    // if the bug exists and it is linked in the specified project then change it
                     if (bug && bug.projectId === project.id) {
-                        // I THINK HERE THE STATUS SHOULD BE HANDLED IN THE FRONT END - user should be able to select if allocated / solved
                         await bug.update(req.body)
                         res.status(202).json({ message: 'accepted' })
                     } else {
                         res.status(404).json({ message: 'bug not found' })
                     }
-                    // else
                 } else {
                     res.status(401).json({ message: 'not authorized. sorry, testers can only create bugs' })
                 }
@@ -581,9 +533,9 @@ apiRouter.put('/users/:uid/projects/:pid/bugs/:bid', async (req, res, next) => {
     }
 })
 
-//Commented - modificat - inlocuit
 
-// GET /bugs/   
+
+
 // apiRouter.get('/projects/:pid/bugs', async (req, res) => {
 //     try {
 //         const project = await Project.findByPk(req.params.pid)
@@ -614,7 +566,7 @@ apiRouter.put('/users/:uid/projects/:pid/bugs/:bid', async (req, res, next) => {
 //     }
 // })
 
-//inlocuit cu
+//temporarly missplaced with
 app.get('/projects/:pid/bugs', async (req, res) => {
     try {
       const project = await Project.findByPk(req.params.pid)
@@ -632,7 +584,6 @@ app.get('/projects/:pid/bugs', async (req, res) => {
   })
 
 
-// Ca MP pot vedea bug-urile înregistrate pentru proiectele din care fac parte.
 
 
 apiRouter.get('/users/:uid/projects/:pid/bugs', async (req, res) => {
